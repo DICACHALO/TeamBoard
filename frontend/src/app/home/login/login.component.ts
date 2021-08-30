cd9f0568bcf5974a03821be766a932ec02752412
Diana Chacon <dchacon871@unab.edu.co>
2021-08-30 18:42:26 -0500

    Feat: list-task

--- frontend/src/app/home/login/login.component.ts
+++ frontend/src/app/home/login/login.component.ts
@@ -41,7 +41,7 @@ export class LoginComponent implements OnInit {
         (res) => {
           console.log(res);
           localStorage.setItem('token', res.jwtToken);
-          this._router.navigate(['/listTasks']);
+          this._router.navigate(['/listTask']);
           this.loginData = {};
         },
         (err) => {
